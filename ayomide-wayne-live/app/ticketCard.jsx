5f98ffe2d079165a5be925e8406b0f3fbd45a139
Godwin Ogu <81978281+ayowayne123@users.noreply.github.com>
2025-02-03 09:10:25 +0000

    Started mobile

--- ayomide-wayne-live/app/ticketCard.jsx
+++ ayomide-wayne-live/app/ticketCard.jsx
@@ -5,7 +5,7 @@ import ticket from "@/public/ticket.png"
 
 function Ticket() {
   return (
-    <div className="relative lg:h-40 w-[300px] sm:w-[380px] h-36 lg:w-[460px]">
+    <div className="relative lg:h-40 w-[300px] sm:w-[350px] h-36 lg:w-[460px]">
 
 <Image src={ticket} alt='ticket' fill className="w-full object-contain   text-white flex items-center justify-center rounded-lg shadow-lg overflow-hidden"/>
     </div>
